0d8078d02616247fa394a7dabb1848f9c86681e9
Priyanka Mane Patil <mane.priyanka39@gmail.com>
2025-05-31 02:37:01 +0530

    Update ProductRepository.cs

--- src/Infrastructure/Repositories/ProductRepository.cs
+++ src/Infrastructure/Repositories/ProductRepository.cs
@@ -1,1 +1,48 @@
+using Core.Entities;
+using Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    /// <summary>
+    /// Provides implementation of IProductRepository using EF Core.
+    /// </summary>
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync() =>
+            await _context.Products.ToListAsync();
+
+        public async Task<Product?> GetByIdAsync(int productId) =>
+            await _context.Products.FindAsync(productId);
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int productId)
+        {
+            var entity = await _context.Products.FindAsync(productId);
+            if (entity != null)
+            {
+                _context.Products.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
 
