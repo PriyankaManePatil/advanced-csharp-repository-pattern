753eb7749bc1cbf4ec5e5c05cdb630b420748ba5
Priyanka Mane Patil <mane.priyanka39@gmail.com>
2025-05-31 02:35:43 +0530

    Update IProductRepository.cs

--- src/Core/Interfaces/IProductRepository.cs
+++ src/Core/Interfaces/IProductRepository.cs
@@ -1,1 +1,17 @@
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    /// <summary>
+    /// Defines contract for product data access.
+    /// </summary>
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllAsync();
+        Task<Product?> GetByIdAsync(int productId);
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+        Task DeleteAsync(int productId);
+    }
+}
 
