ddf2181720e477a78fa0a9c0ad797d66cabd8e8f
Priyanka Mane Patil <mane.priyanka39@gmail.com>
2025-05-31 02:36:11 +0530

    Update AppDbContext.cs

--- src/Infrastructure/AppDbContext.cs
+++ src/Infrastructure/AppDbContext.cs
@@ -1,1 +1,17 @@
+using Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
+namespace Infrastructure
+{
+    /// <summary>
+    /// Represents the Entity Framework Core database context.
+    /// </summary>
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
