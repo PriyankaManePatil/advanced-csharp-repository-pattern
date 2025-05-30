d385ae00f650438996de61234b2005a9cd9fecd3
Priyanka Mane Patil <mane.priyanka39@gmail.com>
2025-05-31 02:35:15 +0530

    Update Product.cs

--- src/Core/Entities/Product.cs
+++ src/Core/Entities/Product.cs
@@ -1,1 +1,12 @@
-
+namespace Core.Entities
+{
+    /// <summary>
+    /// Represents a product entity in the system.
+    /// </summary>
+    public class Product
+    {
+        public int ProductId { get; set; }   // Primary key
+        public string Name { get; set; }     // Product name
+        public decimal Price { get; set; }   // Product price
+    }
+}
